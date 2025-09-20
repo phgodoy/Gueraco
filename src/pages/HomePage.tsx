e598e6b7d2ceadabd424840cdd98c13662c9c819
Phgodoy <phgodoy12@gmail.com>
2025-09-20 19:36:30 -0300

    Inicializa o Projeto Gueraco
    Essa revisão adiciona os arquivos basicos do vite@latestcom a rota estruturada da pagina home.

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-gray-800">
+            GuerAço
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
