da8f8b1697b48aac9d0a20cdcf22549ea6e5cf8d
Phgodoy <phgodoy12@gmail.com>
2025-09-21 13:49:20 -0300

    muda diretorio padrao

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 Caminho base do repositório no GitHub Pages
+  base: "/Gueraco/",
+
   server: {
     host: "::",
     port: 8080,
