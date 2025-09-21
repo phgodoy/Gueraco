1682a3a3bdedd16682d2e1a3b91c201b8103ff0c
Phgodoy <phgodoy12@gmail.com>
2025-09-21 14:00:23 -0300

    Adiciona rota correte e remove favicon

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/Gueraco" element={<LandingPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
